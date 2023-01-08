16dce363557f76b7bdc6e1f04323376d46310ea4
Karolina Marcińczak <carolyn.matusiak90@gmail.com>
2023-01-08 01:53:45 +0100

    biblioteka simple lightbox podpięta

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+//Tworzenie i renderowanie znacznika zgodnie z tablicą danych galleryItems i dostarczonym szablonem elementu galerii.
+galleryItems.forEach((image) => {
+  galleryList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="gallery__item">
+      <a class="gallery__link"
+      href="${image.original}">
+        <img
+        src="${image.preview}" 
+        alt="${image.description}" 
+        class="gallery__image"/>
+      </a>
+    </li>`
+  );
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
